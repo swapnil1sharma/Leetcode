73314d55ea5ec2a18e6fffd711b56b6e60eb9ab4
swapnil1sharma <109137737+swapnil1sharma@users.noreply.github.com>
2024-05-04 13:07:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return false
+        digit = 0
+        temp = x
+        reversed_x = 0
+        while temp > 0:
+            digit = temp % 10
+            reversed_x = reversed_x * 10 + digit
+            temp //= 10
+
+        if x == reversed_x:
+            return True
+        return False
