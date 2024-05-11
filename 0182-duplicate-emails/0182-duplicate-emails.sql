c40261cff71fa4160313bc4dfcbd7ecd395731f4
swapnil1sharma <109137737+swapnil1sharma@users.noreply.github.com>
2024-05-11 14:50:56 +0530

    Time: 781 ms (46.25%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,10 +1,3 @@
 SELECT email from Person
 group by email
 having count(email) > 1;
-
-SELECT DISTINCT(p1.email) from Person p1, Person p2
-where p1.id <> p2.id AND p1.email = p2.email;
-
-SELECT DISTINCT(p1.email) from 
-Person p1 JOIN Person p2 ON
-p1.email = p2.email AND p1.id <> p2.id;
